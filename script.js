fb2370a7cbb240913c3025b10b11626e42e85413
Markandey Shukla <117027551+markandeyshukla@users.noreply.github.com>
2026-01-31 22:57:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function toCase(text) {
+	let text1=text.toUpperCase()
+	return text-text1;
   // write your code here
 }
 
 // DO not change the code below
 
-const text = prompt("Enter text:");
-alert(toCase(text));
+// const text = prompt("Enter text:");
+// alert(toCase(text));
